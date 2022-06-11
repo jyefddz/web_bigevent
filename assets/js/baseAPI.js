0fc9ed4a7d14aa3d56dd4ec688c4c158b695f0de
jyefddz <1065697146@qq.com>
2022-06-11 09:46:35 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
 // 请求拦截器
 $.ajaxPrefilter((options) => {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+    // 注入token
+    if (options.url.includes('/my/')) {
+        options.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+
+    options.complete = (res) => {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
 })
